1405f70904c730e686bf2e7ee4294882572928a8
rouenemedina <rouenemedina@gmail.com>
2024-07-03 19:57:31 -0400

    created a JSON file to better understand my data before making a database tables

---
+++ seed-data/user.js
@@ -0,0 +1,14 @@
+//Data when users successfully signed-up in the website
+//Data when users are logging in {email, password}
+
+export default [
+    {
+        id: 1,
+        first_name: "John",
+        last_name: "Thomas",
+        email: "johnthomas@email.ca",
+        password: "123"
+    },
+];
+
+
