d2dde34e6ae8bebcf018b870ae98a178ff69e2fa
rouenemedina <rouenemedina@gmail.com>
2024-07-04 10:14:45 -0400

    refactored user.js data to match what I created on my tables, created .env files, index and knexfile

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+
+const app = express();
+const PORT = process.env.PORT || 8081;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
--- seed-data/user.js
+++ seed-data/user.js
@@ -3,11 +3,20 @@
 
 export default [
     {
-        id: 1,
-        first_name: "John",
-        last_name: "Thomas",
-        email: "johnthomas@email.ca",
-        password: "123"
+        user_id: 1,
+        user_first_name: "John",
+        user_last_name: "Thomas",
+        user_email: "johnthomas@email.ca",
+        user_password: "123",
+        user_type: "photographer"
+    },
+    {
+        user_id: 2,
+        user_first_name: "Jane",
+        user_last_name: "Watson",
+        user_email: "janew@email.ca",
+        user_password: "123456",
+        user_type: "client"
     },
 ];
 
