6f678173348dbd55fd43b3e6039b4fdc21648952
rouenemedina <rouenemedina@gmail.com>
2024-07-07 00:19:10 -0400

    added cors

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.js";
 const app = express();
 const PORT = process.env.PORT || 8081;
 
+app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoutes);
 
