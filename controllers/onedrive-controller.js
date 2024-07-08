4c8c05e2a0cc427a3ae195e5cf8d8b4dcaaf2307
rouenemedina <rouenemedina@gmail.com>
2024-07-08 17:54:24 -0400

    written what I need to do/get from onedrive api: token and the image url

--- controllers/onedrive-controller.js
+++ controllers/onedrive-controller.js
@@ -1,3 +1,24 @@
-import express from "express";
+import initKnex from "knex";
+import configuration from "../knexfile.js";
 import axios from "axios";
 import fs from "fs";
+import path from "path";
+import multer from "multer";
+import "dotenv/config";
+
+const knex = initKnex(configuration);
+
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const tenantId = process.env.TENANT_ID;
+const onedriveFolderPath = process.env.ONEDRIVE_FOLDER_PATH;
+
+//get Access Token
+const getAccessToken = async () => {
+
+};
+
+//upload images to OneDrive
+//then get the image url from OneDrive
+
+//POST /upload
---
+++ routes/onedrive.js
@@ -0,0 +1,8 @@
+import express from "express";
+import * as onedriveController from "../controllers/onedrive-controller.js";
+
+const router = express.Router();
+
+router.route("/")
+
+export default router;
